1f2ce7984f9194ac8b888dd1b160acf7f0130043
Sebastian Oehne <Sebastian-Oehne@web.de>
2021-05-09 17:57:26 +0200

    Fill
    Fill

---
+++ Spieleprogrammierung_SebastianOehne_1582940/Assets/LightSwitch.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+
+	public GameObject light;
+	private bool on = false;
+
+
+	void OnTriggerStay(Collider plyr) {
+
+		if(plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E) && !on){
+			
+			light.SetActive(true);
+			on = true;
+
+		}
+		
+		else if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E) && on){
+
+			light.SetActive(false);
+			on = false;
+
+		}
+	}
+}
